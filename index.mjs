17645ceb26b2c4eb846fd8573b428631134efe70
Shahriar Hasan <hasanshahriar32@gmail.com>
2024-12-02 12:32:01 +0000

    Update Puppeteer import and modify browser launch configuration

--- index.mjs
+++ index.mjs
@@ -1,12 +1,13 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 
 async function main() {
   const browser = await puppeteer.launch({
-    headless: false
+    executablePath: './.cache/puppeteer/chrome/linux-131.0.6778.85/chrome-linux64/chrome',
+    // headless: false
   });
 
   const page = await browser.newPage();
-  await page.goto('https://shahriarhasan.vercel.app');
+  await page.goto('https://example.com');
   const version = await browser.version();
   await browser.close();
   console.log({ version });
